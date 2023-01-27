931cecbf715a093941a79104c66057f6f7b7facb
Adrian-Nicolae Dediu <111912660+adrdediu@users.noreply.github.com>
2023-01-27 13:52:29 +0200

    Added a readme file and modified the gitignore to add the amplify folder

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifydemoapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifydemoapp462393f6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3f38fbbe9": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Amplify-demo app
+
+Welcome to the amplify-demo GitHub project! This project is a demonstration of using the AWS Amplify framework to build a full-stack web application.
+
+The application is a simple notes management tool that allows users to create, read, update, and delete tasks. It is built using React for the frontend and AWS Amplify for the backend.
+
+Check out the working demo of the app here: https://main.d3o101epuend7x.amplifyapp.com/
+
+I hope you find this demonstration helpful and informative. If you have any questions or feedback, feel free to open an issue on this GitHub repository.
